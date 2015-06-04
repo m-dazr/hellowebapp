04850dda4bd7c26c365b98f895052fbf295f3ada
m-dazr <spazeman31@gmail.com>
2015-06-04 20:39:29 +0800

    Admin,models

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from collection.models import Post
+
+class PostAdmin(admin.ModelAdmin):
+	model = Post
+	list_display = ('title', 'text')
+	prepopulated_fields = {'slug': ('title',)}
+
+
+
+admin.site.register(Post, PostAdmin)
