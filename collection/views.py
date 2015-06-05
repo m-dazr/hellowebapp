00d5b754658cd55c3e1934738b09bf14f2161280
m-dazr <spazeman31@gmail.com>
2015-06-05 16:16:37 +0800

    updated urls, view

--- collection/views.py
+++ collection/views.py
@@ -6,3 +6,13 @@ def index(request):
    # this is your new view
    posts = Post.objects.all()
    return render(request, 'index.html', {'posts': posts,})
+
+def post_detail(request, slug):
+
+	#grab object
+	post = Post.objects.get(slug=slug)
+
+	#pass to template
+	return render(request, 'posts/post_detail.html', {'post': post,})
+
+
--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     url(r'^$', 'collection.views.index', name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
+    url(r'^posts/(?P<slug>[-\w]+)/$', 'collection.views.post_detail', name='post_detail'),
     url(r'^admin/', include(admin.site.urls)),
 )
